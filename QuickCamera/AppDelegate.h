df49217ece41b191246afe28181ba47d23a3c7c4
Rajat Gupta <rajat@braintechnosys.com>
2016-02-04 11:58:17 +0530

    Initial Commit

---
+++ QuickCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickCamera
+//
+//  Created by Arpit on 2/4/16.
+//  Copyright © 2016 Apcog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickCamera
+//
+//  Created by Arpit on 2/4/16.
+//  Copyright © 2016 Apcog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
